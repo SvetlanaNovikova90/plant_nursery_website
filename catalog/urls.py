ed54022d15769c0aeef22b398fd21aeca8e7093d
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-05-07 00:59:01 +0300

    '20.2'

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from catalog.views import home, contact
+from catalog.views import home, contact, products, products_detail
 
 urlpatterns = [
     path("", home),
     path("contact/", contact),
+    path("products/", products),
+    path("product/<int:pk>/", products_detail, name='products_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
@@ -12,3 +14,16 @@ def contact(request):
         message = request.POST.get("message")
         print(f"{name} ({phone}): {message}")
     return render(request, "catalog/contact.html")
+
+
+def products(request):
+    product = Product.objects.all()
+    context = {'object_list': product}
+    return render(request, "catalog/products.html", context)
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, "catalog/product_detail.html", context)
+
+
