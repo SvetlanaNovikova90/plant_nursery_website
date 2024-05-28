873649eceb6b82e3b5f84ca1cc97259fc2c3d2ca
Svetlana Novikova <seaciliya2012@yandex.ru>
2024-05-28 22:21:36 +0300

    '22.2 2'

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("catalog.urls", 'shop'), namespace='shop')),
+    path("users/", include(("users.urls", 'users'), namespace='users')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
